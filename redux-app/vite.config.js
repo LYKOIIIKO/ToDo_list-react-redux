5cff1fb19457a4bd1e81bcb8684ddd9fdc1c424e
Anton Haba <lykoiiiko@gmail.com>
2025-06-11 10:33:11 +0300

    fix ts1343 error

--- redux-app/vite.config.js
+++ redux-app/vite.config.js
@@ -6,7 +6,7 @@ import EnvironmentPlugin from 'vite-plugin-environment';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), EnvironmentPlugin('all')],
+  plugins: [react(), svgr(), EnvironmentPlugin(['VITE_FIREBASE_API_KEY', 'VITE_FIREBASE_AUTH_DOMAIN','VITE_FIREBASE_PROJECT_ID','VITE_FIREBASE_STORAGE_BUCKET','VITE_FIREBASE_MESSAGING_SENDER_ID','VITE_FIREBASE_APP_ID'])],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
